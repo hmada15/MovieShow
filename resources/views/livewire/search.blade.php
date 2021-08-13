00589158405e44938a855c06e36b7ea826454269
hmada15 <31375621+hmada15@users.noreply.github.com>
2021-08-13 19:14:39 +0200

    fix wrong link

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,6 +1,7 @@
 <div>
     <form class="form-inline my-2 my-lg-0">
-        <input wire:model.depounce.300ms="search" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <input wire:model.depounce.300ms="search" class="form-control mr-sm-2" type="search" placeholder="Search"
+            aria-label="Search">
         <button class="btn btn-outline-primary my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
     </form>
     <div class="position-absolute search-drop">
@@ -8,14 +9,14 @@
             @isset($movies['results'])
                 @foreach ($movies['results'] as $movie)
                     <li>
-                        <a href="{{route("movie.show",[$movie['id'],SlugIt($movie['title'])])}}">
-                            @isset($movie["poster_path"])
-                            <img src="https://image.tmdb.org/t/p/w45/.{{$movie["poster_path"]}}" />
+                        <a href="{{ route('movie.show', [$movie['id'], SlugIt($movie['title'])]) }}">
+                            @isset($movie['poster_path'])
+                                <img src="https://image.tmdb.org/t/p/w45/.{{ $movie['poster_path'] }}" />
                             @else
-                            <img src="{{asset("img/movi_placeholder.png")}}">
+                                <img src="{{ asset('img/movi_placeholder.png') }}">
                             @endisset
-                            <span>{{$movie["title"]}}</span>
-                            <p>{{$movie["release_date"] ??''}}</p>
+                            <span>{{ $movie['title'] }}</span>
+                            <p>{{ $movie['release_date'] ?? '' }}</p>
                         </a>
                     </li>
                     @if ($loop->iteration == 3) @break @endif
@@ -25,14 +26,14 @@
             @isset($tv_shows['results'])
                 @foreach ($tv_shows['results'] as $tv)
                     <li>
-                        <a href="{{route("movie.show",[$tv['id'],SlugIt($tv['name'])])}}">
-                            @isset($tv["poster_path"])
-                            <img src="https://image.tmdb.org/t/p/w45/.{{$tv["poster_path"]}}" />
+                        <a href="{{ route('tv.show', [$tv['id'], SlugIt($tv['original_name'])]) }}">
+                            @isset($tv['poster_path'])
+                                <img src="https://image.tmdb.org/t/p/w45/.{{ $tv['poster_path'] }}" />
                             @else
-                            <img src="{{asset("img/movi_placeholder.png")}}">
+                                <img src="{{ asset('img/movi_placeholder.png') }}">
                             @endisset
-                            <span>{{$tv["name"]}}</span>
-                            <p>{{$tv["first_air_date"] ?? ''}}</p>
+                            <span>{{ $tv['name'] }}</span>
+                            <p>{{ $tv['first_air_date'] ?? '' }}</p>
                         </a>
                     </li>
                     @if ($loop->iteration == 3) @break @endif
@@ -43,4 +44,3 @@
 
     </div>
 </div>
-
